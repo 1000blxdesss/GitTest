0b0d09df4d2500adde0de65ac4b0f28c8319df77
1000blxdesss <147636287+1000blxdesss@users.noreply.github.com>
2024-10-09 19:45:30 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import main
 import numpy
 
 def test_add():
-    assert main.add(1, 2) == 1
+    assert main.add(1, 2) == 3
     assert main.add(-1, 1) == 0
     assert main.add(-1, -1) == -2
     assert main.add(-1, 0) == -1
